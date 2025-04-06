3ae1316e16e38fd09b37fecb0427e18b9fba19c7
root <root@LAPTOP-BTRRFLJO.>
2025-04-06 14:48:15 +1000

    Add new project

---
+++ src/main/java/org/orlov/tom/entity/Note.java
@@ -0,0 +1,25 @@
+package org.orlov.tom.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    public void setNotes(String text)
+    {
+        this.text = text;
+    }
+
+    public String getNotes() {
+        return text;
+    }
+}
